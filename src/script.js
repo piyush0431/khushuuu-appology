917b38e40260b8b266458a5ec17c8519aa8b78b4
piyush0431 <piyush31032005@gmail.com>
2024-03-18 02:33:46 +0530

    hope she'll be happy

--- src/script.js
+++ src/script.js
@@ -1,8 +1,12 @@
-
 onload = () => {
+    document.getElementById('sonaa').style.display = 'block';
+};
+
+document.getElementById('sonaa').onclick = (function () {
+    document.getElementById('sonaa').remove()
     const c = setTimeout(() => {
         document.body.classList.remove("not-loaded");
 
         clearTimeout(c);
     }, 1000);
-};
+});
